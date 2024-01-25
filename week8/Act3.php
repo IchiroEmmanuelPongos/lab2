167e40e82d897ac443a520f7b2d60a3ec84b3c50
Ichiro Emmanuel Pongos <ipongos@student.apc.edu.ph>
2024-01-25 16:08:54 +0800

    Variables and Operators

--- week8/Act3.php
+++ week8/Act3.php
@@ -15,10 +15,28 @@ echo "<h1>Pongos, Ichiro Emmanuel</h1>";
 <body>
 
 <?php
+// introduction
 echo "<h2>PHP is Fun!</h2>";
 echo "Hello world!<br>";
 echo "I'm about to learn PHP!<br>";
 ?> 
+
+<?php
+// variables and operators
+$testtitle = "Variables";
+$txt2 = "x is 10 and y is 6, Summed to: ";
+$x = 10;
+$y = 6;
+$Total = $x + $y;
+
+echo "<h3>" . $testtitle . "</h3>";
+
+echo "x = " . $x . "<br>";
+echo "y = " . $y . "<br>";
+echo $txt2 . $Total . "<br>";
+echo "x + y";
+?> 
+
 
 
 </body>
